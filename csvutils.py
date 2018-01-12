5cea4d908bbe825c8ea1a53266578659e5bfa579
Declan Valters <declan.valters@ed.ac.uk>
2018-01-12 17:30:34 +0000

    Unit test for csv utils

--- csvutils.py
+++ csvutils.py
@@ -7,6 +7,8 @@ DAV January 2018
 """
 
 import csv
+import unittest
+import pickle
 
 def show_csv_headers(csvfile):
   """Reads the header from a csv file and prints
@@ -18,10 +20,27 @@ def show_csv_headers(csvfile):
      Returns:
         A list of header column names
      """
+  listy = []
   with open(csvfile) as f:
     reader = csv.reader(f)
     i = next(reader)
     print i
+    listy.append(i)
+  return listy
 
+class TestCsvReaders(unittest.TestCase):
 
+  def test_readsimplecsvheader(self):
+    
+    csv_mock = [('foo', 'bar', 'baz')] 
+    with open("test.csv", 'wb') as csvtest:
+      wr = csv.writer(csvtest, dialect='excel')
+      wr.writerows(csv_mock)
+
+    self.assertEqual(show_csv_headers("test.csv"), [['foo', 'bar', 'baz']])
+
+if __name__=='__main__':
+  unittest.main()
+
+    
 
